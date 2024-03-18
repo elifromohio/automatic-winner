4d5fb60318ae1149379c6a0f710aa608b28c0675
Elijah Addleman <elijahaddleman@gmail.com>
2024-03-17 18:20:01 -0700

    added sprites and started putting them in

--- game.py
+++ game.py
@@ -6,12 +6,15 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
+background = pygame.image.load("media/Sky.png")
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    # draw all elements
-    # update everything
+    
+    screen.blit(background,(0,0))
+
     pygame.display.update()
     clock.tick(60)
