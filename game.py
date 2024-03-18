f76056091b4eb38c73e6a4635fdaed79e174da84
Elijah Addleman <elijahaddleman@gmail.com>
2024-03-17 20:13:03 -0700

    Made landscape and learnd about text files.

--- game.py
+++ game.py
@@ -5,8 +5,13 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
+font = pygame.font.Font("media/hand_marker.otf", 50)
 
 background = pygame.image.load("media/Sky.png")
+ground = pygame.image.load("media/ground.png")
+bg_image_big = pygame.transform.scale(background, (800,300))
+bg_image_big2 = pygame.transform.scale(ground, (800,100))
+text_surface = font.render("My game", False, "Green")
 
 while True:
     for event in pygame.event.get():
@@ -14,7 +19,9 @@ while True:
             pygame.quit()
             exit()
     
-    screen.blit(background,(0,0))
+    screen.blit(bg_image_big,(0,0))
+    screen.blit(bg_image_big2,(0,300))
+    screen.blit(text_surface,(300,50))
 
     pygame.display.update()
     clock.tick(60)
