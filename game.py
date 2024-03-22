7b237d528e113f0b752b5dff9d66ce3d075bf93b
Elijah Addleman <elijahaddleman@gmail.com>
2024-03-22 13:25:53 -0700

    updated alot and added new sprites tried to add a snail animation, but failed miserably and made the game not function whatsoever. im proud :).

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+import time, os, random
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
@@ -9,16 +10,18 @@ font = pygame.font.Font("media/hand_marker.otf", 50)
 
 background = pygame.image.load("media/Sky.png").convert()
 ground = pygame.image.load("media/ground.png").convert()
-text_surface = font.render("Running Game", False, "Green").convert()
 
-snail = pygame.image.load("media/snail.png").convert_alpha()
-snail_x_pos = 600
 
-# pc = pygame.image.load()
+
 
 bg_image_big = pygame.transform.scale(background, (800,300))
 bg_image_big2 = pygame.transform.scale(ground, (800,100))
-bg_image_big3 = pygame.transform.scale(snail, (50,50))
+
+snail_frame = 1
+while snail_frame == 1:
+    snail = pygame.image.load("media/snail.png").convert_alpha()
+while snail_frame == 1:
+    snail = pygame.image.load("media/snail_2.png").convert_alpha()
 
 
 while True:
@@ -29,12 +32,19 @@ while True:
     
     screen.blit(bg_image_big,(0,0))
     screen.blit(bg_image_big2,(0,300))
-    screen.blit(text_surface,(300,50))
-    snail_x_pos -= 4
-    if snail_x_pos < 1:
-        snail_x_pos = 800
-    screen.blit(bg_image_big3,(snail_x_pos,257))
+    snail_frame = 0
+    while True:
+        time.sleep(0.5)
+        snail_frame == 2
+        time.sleep(0.5)
 
-    pygame.display.update()
-    clock.tick(60)
-    mouse = pygame.mouse.get_pos()
+        bg_image_big3 = pygame.transform.scale(snail, (50,50))
+        snail_x_pos = 600
+        snail_x_pos -= 4
+        if snail_x_pos < 1:
+            snail_x_pos = 800
+        screen.blit(bg_image_big3,(snail_x_pos,257))
+
+        pygame.display.update()
+        clock.tick(60)
+        mouse = pygame.mouse.get_pos()
